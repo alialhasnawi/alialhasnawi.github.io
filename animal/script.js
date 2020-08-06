2a19d09747b553d1f94e2b6be659945ab4b04fd6
Ali Al-Hasnawi <44278853+alialhasnawi@users.noreply.github.com>
2020-08-06 17:06:49 -0400

    Added GUI

--- animal/script.js
+++ animal/script.js
@@ -1,136 +1,191 @@
+//constants of accepted sound files and filepath main directory
+const phonemes = ['a', 'e', 'i', 'o', 'u', 'b', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'C', 'S', 'T', 'U'];
+const voicePath = 'res/voice/';
+
+// web audio api
+var audioContext;
+var sounds = [];
+
+// html DOM variables
+var dialogue;
+var rateSlider;
+var inText;
+
+// init variables for speak()
+var speaking = false;
+var speakingIntervals = [];
+var globalRate = 1.8;
+
 document.addEventListener('DOMContentLoaded', function(e) {
+  dialogue = document.getElementById('dialogue-text');
+  slider = document.getElementById('rateRange');
+  inText = document.getElementById('inText');
 
-  const phonemes = ['a', 'e', 'i', 'o', 'u', 'b', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'C', 'S', 'T', 'U'];
-  const voicePath = 'res/voice/';
-  const srctxt = "THE FUCK YOU SAY TO ME, YOU LITTLE SHIT? AHAHAHAHAHA! HOW ARE YOU- HOW ARE YOU NOT IN FUCKING SCHOOL? YOU KISS YOUR MOTHER WITH THAT MOUTH? IT'S CALLED \" YOU KISS - \" IT'S CALLED \" YOU KISS YOUR MOTHER WITH THAT FUCKING MOUTH ? \" HUH? Huh? AHAHAHA- HE'S SO- AGAHAHAHAH BECAUSE THE FUCKING YOUTH OF SOCI- AAAAAAAAAAHAHAAAAAAAAAAAAAAHAAHAHAAAAAAA AAA- YOU SHUT UP WHEN I'M TALKING TO YOU! YOU SHUT YOUR MOUTH!";
-
-  var audioContext;
-  var sounds = [];
-
-  var pTag = document.getElementsByTagName('p')[0];
-
-
-  function init() {
-    // inits context on click
-    audioContext = new AudioContext();
-
-    // loads sounds
-    loadInventory();
-
-    // prevents duplicate inits
-    document.removeEventListener('click', init);
-
-    console.log(toAnimal(srctxt));
-    console.log(sounds);
-
-    speak(srctxt, 70, 2.2);
-  }
-
-  function speak(text, time, rate) {
-    str = toAnimal(text);
-    length = str.length;
-    originalLength = text.length;
-
-    setLimitedInterval(function(i) {
-      if (phonemes.includes(str[i])) {
-        play(sounds[str[i]], rate);
-      } else if (str[i] == ' ') {
-        // nothing
-      } else {
-        // wait
-      }
-      i++;
-    }, time, length);
-
-
-
-    setLimitedInterval(function(i) {
-      pTag.innerHTML += text[i];
-    }, Math.round((length / originalLength) * time), originalLength);
-  }
-
-  function play(buffer, rate) {
-    const source = audioContext.createBufferSource();
-    source.buffer = buffer;
-    source.playbackRate.value = rate;
-    source.connect(audioContext.destination);
-    source.start();
-  }
-
-  function setLimitedInterval(func, time, iterations) {
-    var i = 0;
-
-    var id = setInterval(function() {
-      func(i);
-      i++;
-
-      if (i === iterations) {
-        clearInterval(id);
-      }
-    }, time);
-  }
-
-  function toAnimal(str) {
-    temp = str.toLowerCase();
-
-    temp = temp.replace('c', 'k');
-    temp = temp.replace('q', 'k');
-    temp = temp.replace('wh', 'w');
-    temp = temp.replace('ph', 'f');
-    temp = temp.replace('ch', 'C');
-    temp = temp.replace('sh', 'S');
-    temp = temp.replace('th', 'T');
-    temp = temp.replace('oo', 'U');
-
-    temp = temp.replace('.', '    ');
-    temp = temp.replace(',', '    ');
-    temp = temp.replace('!', '    ');
-    temp = temp.replace('?', '    ');
-
-    return temp;
-  }
-
-  // loads sound buffer from path
-  async function load(path) {
-    const response = await fetch('./' + path);
-    const arrayBuffer = await response.arrayBuffer();
-    return await audioContext.decodeAudioData(arrayBuffer);
-  }
-
-  // adds sound to array
-  function addSound(path, index) {
-    load(path).then(response => {
-      sounds[index] = response;
-    });
-    return index;
-  }
-
-  function loadInventory() {
-    phonemes.forEach((item, index) => {
-      var file;
-
-      switch (item) {
-        case 'C':
-          file = 'ch.ogg';
-          break;
-        case 'S':
-          file = 'sh.ogg';
-          break;
-        case 'T':
-          file = 'th.ogg';
-          break;
-        case 'U':
-          file = 'oo.ogg';
-          break;
-        default:
-          file = item + '.ogg';
-      }
-
-      addSound(voicePath + file, item);
-    });
-
+  slider.oninput = function() {
+    globalRate = 0.4 * Math.pow(4, this.value) + 0.2;
   }
 
   document.addEventListener('click', init);
-
 });
+
+function init() {
+  // inits context on click
+  audioContext = new AudioContext();
+
+  // loads sounds
+  loadInventory();
+
+  // prevents duplicate inits
+  document.removeEventListener('click', init);
+}
+
+function playClip() {
+  shut();
+  if (inText.value !== '') {
+    speak(inText.value, 70, globalRate);
+  }
+}
+
+function shut() {
+  speakingIntervals.forEach((item, i) => {
+    clearInterval(item);
+  });
+
+  speaking = false;
+  speakingIntervals = [];
+}
+
+function speak(text, time, rate, ignore = false) {
+  if (!speaking || ignore) {
+    speaking = true;
+    var str = toAnimal(text);
+
+    var length = str.length;
+    var originalLength = text.length;
+
+    var speakingID = setLimitedInterval(function(i) {
+      if (phonemes.includes(str[i])) {
+        play(sounds[str[i]], globalRate);
+      } else if (str[i] == ' ') {
+        // nothing
+      } else {
+        play(sounds['b'], globalRate);
+      }
+      i++;
+    }, time, length, null, function() {
+      speaking = false
+    });
+
+    var typingID = setLimitedInterval(function(i) {
+      dialogue.innerHTML += text[i];
+    }, Math.round((length / originalLength) * time), originalLength, function() {
+      dialogue.innerHTML = '';
+    });
+
+    speakingIntervals.push(speakingID, typingID);
+  }
+
+  return {
+    'text': text,
+    'time': time,
+    'rate': rate,
+    'ignore': ignore
+  };
+}
+
+function setLimitedInterval(func, time, iterations, beginning = null, ending = null) {
+  var i = 0;
+
+  if (beginning !== null) {
+    beginning();
+  }
+
+  var id = setInterval(function() {
+    func(i);
+    i++;
+
+    if (i === iterations) {
+      if (ending !== null) {
+        ending();
+      }
+      clearInterval(id);
+    }
+  }, time);
+
+  return id;
+}
+
+function toAnimal(str) {
+  return str.toLowerCase()
+    .replace(/wh/g, 'w')
+    .replace(/ph/g, 'f')
+    .replace(/ch/g, 'C')
+    .replace(/sh/g, 'S')
+    .replace(/th/g, 'T')
+    .replace(/ss/g, 's')
+    .replace(/oo/g, 'U')
+    .replace(/c/g, 'k')
+    .replace(/q/g, 'k')
+    .replace(/\./g, '      ')
+    .replace(/,/g, '      ')
+    .replace(/!/g, '      ')
+    .replace(/\?/g, '      ')
+    .replace(/1/g, 'wan')
+    .replace(/2/g, 'tU')
+    .replace(/3/g, 'Tri')
+    .replace(/4/g, 'for')
+    .replace(/5/g, 'fayv')
+    .replace(/6/g, 'six')
+    .replace(/7/g, 'seven')
+    .replace(/8/g, 'eyt')
+    .replace(/9/g, 'nayn');
+}
+
+
+function loadInventory() {
+  phonemes.forEach((item, index) => {
+    var file;
+
+    switch (item) {
+      case 'C':
+        file = 'ch.ogg';
+        break;
+      case 'S':
+        file = 'sh.ogg';
+        break;
+      case 'T':
+        file = 'th.ogg';
+        break;
+      case 'U':
+        file = 'oo.ogg';
+        break;
+      default:
+        file = item + '.ogg';
+    }
+
+    addSound(voicePath + file, item);
+  });
+}
+
+function play(buffer, rate) {
+  const source = audioContext.createBufferSource();
+  source.buffer = buffer;
+  source.playbackRate.value = rate;
+  source.connect(audioContext.destination);
+  source.start();
+}
+
+// loads sound buffer from path
+async function load(path) {
+  const response = await fetch('./' + path);
+  const arrayBuffer = await response.arrayBuffer();
+  return await audioContext.decodeAudioData(arrayBuffer);
+}
+
+// adds sound to array
+function addSound(path, index) {
+  load(path).then(response => {
+    sounds[index] = response;
+  });
+  return index;
+}
