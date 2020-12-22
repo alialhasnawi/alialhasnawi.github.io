75457b0fcdcd306beff98b7c6575266e7e013eba
Ali Al-Hasnawi <44278853+alialhasnawi@users.noreply.github.com>
2020-12-22 18:36:53 -0500

    Pointer testing 3

--- crismon-countdown/js/1.js
+++ crismon-countdown/js/1.js
@@ -1,8 +1,10 @@
-var fingyCount = 0;
+let fingyCount = 0;
+
+console.log((navigator.maxTouchPoints > 1 ? 'touchend' : 'pointerup'));
 
 window.addEventListener((navigator.maxTouchPoints > 1 ? 'touchend' : 'pointerup'), (e) => {
   if (fingyCount < 7) {
-    console.log(`${e.clientX}, ${e.clientY}, ${e.twist || e.rotationAngle}`);
+    console.log(`${e.clientX}, ${e.clientY}, ${(navigator.maxTouchPoints > 1 ? e.rotationAngle : e.twist)}`);
 
     const chicken = document.createElement('div');
     chicken.className = 'chicken';
@@ -14,7 +16,7 @@ window.addEventListener((navigator.maxTouchPoints > 1 ? 'touchend' : 'pointerup'
 
     chicken.appendChild(thumb);
 
-    chicken.style.transform = `translate(${e.clientX}px, ${e.clientY}px) rotate(${e.twist || e.rotationAngle}deg)`;
+    chicken.style.transform = `translate(${e.clientX}px, ${e.clientY}px) rotate(${(navigator.maxTouchPoints > 1 ? e.rotationAngle : e.twist)}deg)`;
 
     document.body.appendChild(chicken);
 
