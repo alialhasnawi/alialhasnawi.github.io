4852bff33dba86b9cde7329aa5abc841a9d3c3da
Ali Al-Hasnawi <44278853+alialhasnawi@users.noreply.github.com>
2020-12-23 17:33:01 -0500

    Pointer testing 5

---
+++ crismon-countdown/js/2.js
@@ -0,0 +1,80 @@
+const grassTemplate = '<div onpointerenter="grassEnter(this);" onpointerleave="grassLeave(this);" class="grass-container"><img class="grass" src="res/grass.png" alt="grass"></div>';
+
+document.querySelector('.grass-grid').innerHTML = grassTemplate.repeat(25);
+
+grassReady = false;
+firstTouch = true;
+
+const before = document.querySelector('#before');
+const after = document.querySelector('#after');
+
+const scrolling = document.querySelector('.scrolling');
+
+function grassEnter(element) {
+  //console.log('entered!');
+  //element.style.transform = 'scaleY(0.8)';
+  element.childNodes[0].style.height = '15vw';
+
+  if (firstTouch) {
+    window.setTimeout(() => {
+      grassReady = true;
+    }, 6500);
+
+    firstTouch = false;
+  }
+
+  if (grassReady) {
+    console.log('cool shit');
+    grassReady = false;
+
+    document.querySelector('#music').play();
+
+    //do next anim
+    document.body.style.backgroundColor = '#000';
+    before.style.opacity = '0';
+
+    setTimeout(() => {
+      before.style.display = 'none';
+    }, 1000);
+
+    setTimeout(() => {
+      after.style.display = 'flex';
+      document.body.style.backgroundColor = '#d5e7e8';
+      after.style.opacity = '1';
+
+      setTimeout(() => {
+        scrolling.style.width = '100%';
+
+        setTimeout(() => {
+          scrolling.style.width = '0%';
+
+          setTimeout(() => {
+            scrolling.innerHTML = 'mery cristamAS!! hehe';
+            scrolling.style.width = '100%';
+
+            //christmas delay
+          }, 3000);
+
+          //song end delete text delay
+        }, 10000);
+
+        //text appear delay
+      }, 1000);
+
+      //after appear delay
+    }, 4000);
+  }
+}
+
+function grassLeave(element) {
+  //element.style.transform = '';
+  element.childNodes[0].style.height = '20vw';
+}
+
+function jumpPokemon(pokemon) {
+  pokemon.style.transform = 'translateY(-10px)';
+}
+
+function idlePokemon(pokemon) {
+  pokemon.style.transform = 'translateY(0px)';
+}
